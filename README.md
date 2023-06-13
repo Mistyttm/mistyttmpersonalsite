daec5c13c5f3ba50d288c9251367bdbed07fad86
Mistyttm <emmey.leo@gmail.com>
2023-06-14 00:50:39 +1000

    Home page

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# mistyttmpersonalsite
+# Misty Rose!
 The code source for my personal website built with react and vite, hosted on vercel
+
+## Home Page
+![Home page](/public/home_page.png)
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import NavbarHome from "../components/navbar-home";
+import { ReactComponent as PawPrint } from "../assets/images/Paw_Print_optimized.svg";
 
 function Home() {
     return (
         <div>
-            <NavbarHome />
-            <img src="../../public/logo.png" alt="Misty Rose! logo" className="scale-50" />
+            <PawPrint fill="#C897E626" className="z-0 absolute scale-90 rotate-45 -ml-20 -mt-96" />
+            <div className="z-1">
+                <NavbarHome />
+                <div className="flex flex-col">
+                    <img
+                        src="../../public/hero_logo.png"
+                        alt="Misty Rose! logo"
+                        className="scale-50 justify-self-center -mb-20"
+                    />
+                    <h3 className="font-bagel text-4xl text-center">
+                        Puppy Programmer Girl
+                    </h3>
+                </div>
+            </div>
         </div>
     );
 }
