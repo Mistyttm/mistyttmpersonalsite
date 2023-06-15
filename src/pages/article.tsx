f4a66c7ad703a09aac27c9baf29ccfa2a5962b1d
Mistyttm <emmey.leo@gmail.com>
2023-06-15 22:01:35 +1000

    add an article page for the blog to redirect to

--- src/pages/article.tsx
+++ src/pages/article.tsx
@@ -1,24 +1,39 @@
 import { ReactComponent as PawPrint } from "../assets/images/Paw_Print_optimized.svg";
 import { useEffect } from "react";
 import Navbar from "../components/navbar";
-import { Link } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
+import Error from "./404";
 
-export default function Article() {
+export default function Blog() {
     useEffect(() => {
         document.title = "Blog - Misty Rose";
     }, []);
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const params = searchParams.get("b");
+
+    if (!params) {
+        return (
+            <Error location="/blog" place="to the blog" />
+        )
+    }
+
     return (
         <div className="mx-auto overflow-clip">
             <div className="overflow-clip -z-10 absolute right-0 mt-20 -mr-40">
                 <PawPrint fill="#C897E626" className="scale-90 rotate-30" />
             </div>
+            <div className="-z-10 absolute -ml-60 -mt-96">
+                <PawPrint fill="#C897E626" className="scale-90 -rotate-30" />
+            </div>
             <div className="flex flex-col justify-center place-content-center ">
                 <div className="flex z-5 mb-10">
                     <Navbar />
                 </div>
-                <h1 className="font-bagel text-7xl text-center mb-10">Blog</h1>
+                <h1 className="font-bagel text-7xl text-center mb-10">{params}</h1>
                 <section className="text-center text-xl max-w-3xl ml-auto mr-auto">
-                    <Link to="/blog/article?b=12">Tester</Link>
+                    <p className="">Under construction</p>
                 </section>
             </div>
         </div>
