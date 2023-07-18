d9974380d44c994bb6f4470768a8c4985bb84e6e
Mistyttm <emmey.leo@gmail.com>
2023-07-18 23:23:49 +1000

    Animation

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,10 @@
 import NavbarHome from "../components/navbar-home";
 import { ReactComponent as PawPrint } from "../assets/images/Paw_Print_optimized.svg";
 import { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+Aos.init();
 
 function Home() {
     useEffect(() => {
@@ -14,14 +18,18 @@ function Home() {
             <div className="z-5">
                 <NavbarHome />
                 <div className="flex flex-col">
-                    <img
-                        src="/hero_logo.png"
-                        alt="Misty Rose! logo"
-                        className="scale-50 justify-self-center -mb-20"
-                    />
-                    <h3 className="font-bagel text-4xl text-center">
-                        Puppy Programmer Girl
-                    </h3>
+                    <div data-aos="fade-up">
+                        <img
+                            src="/hero_logo.png"
+                            alt="Misty Rose! logo"
+                            className="scale-50 justify-self-center -mt-10 -mb-20"
+                        />
+                    </div>
+                    <div data-aos="fade-up">
+                        <h3 className="font-bagel text-4xl text-center">
+                            Puppy Programmer Girl
+                        </h3>
+                    </div>
                 </div>
             </div>
         </div>
